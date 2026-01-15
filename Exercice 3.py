e6d25fe6a26c9644d2714774027034c6f221cf42
ibranar193 <mouadkadmiri@gmail.com>
2026-01-15 15:31:32 -0500

    3 - exercice 3 : fix min + fix max

---
+++ Exercice 3.py
@@ -0,0 +1,12 @@
+from Outils import Outils
+
+entier = Outils()
+
+entier.saisir()
+print("-------------------------------------------------------------")
+print(entier)
+print("-------------------------------------------------------------")
+print(f"Minimum :{entier.min()}")
+print(f"Maximum :{entier.max()}")
+print(f"Somme :{entier.somme()}")
+print(f"Moyenne :{entier.moyenne()}")
--- Outils.py
+++ Outils.py
@@ -8,15 +8,27 @@ class Outils :
         for i in range(10) :
             entier = int (input(f"Nombre {i+1} : "))
             self.entiers.append(entier)
+        self.array = np.array(self.entiers)
 
     def min(self) :
-        return self.array.min()
+        minimum = self.entiers[0]
+        for i in self.entiers :
+            if i < minimum :
+                minimum = i
+        return minimum
 
     def max(self) :
-        return self.array.max()
+        maximum = self.entiers[0]
+        for i in self.entiers:
+            if i >maximum:
+                maximum = i
+        return maximum
 
     def somme(self) :
         return self.array.sum()
 
     def moyenne(self) :
         return self.array.mean()
+
+    def __str__(self):
+        return f"{self.entiers}"
