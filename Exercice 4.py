de4d8cc6afaf7d613b9c880746ccb0c7fdcf5ff4
ibranar193 <mouadkadmiri@gmail.com>
2026-01-15 17:54:05 -0500

    cree le class

---
+++ Exercice 4.py
@@ -0,0 +1,10 @@
+# Class Etudiant
+class Etudiant:
+    def __init__(self, nom):
+        self.nom = nom
+        self.notes = [15, 18, 12]
+        self.identite = (nom, "2025-H")
+        self.resultats = {"POO": 90, "Cisco": 85}
+
+
+
