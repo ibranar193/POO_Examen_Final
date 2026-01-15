64893a0d0f845df02a4cc98f1b653860d12ee63d
ibranar193 <mouadkadmiri@gmail.com>
2026-01-15 13:57:00 -0500

    3 - commit : text + lineEdit

--- Exercice 2.py
+++ Exercice 2.py
@@ -1,11 +1,18 @@
 # Importer la bibliotheque
-from PyQt6.QtWidgets import QApplication, QWidget
-
+from PyQt6.QtWidgets import QApplication, QWidget, QGridLayout, QLabel, QLineEdit
 
 app = QApplication([])
 fen = QWidget()
 fen.setWindowTitle("Doubleure")
 fen.setGeometry(100, 100, 400, 200)
+
+text1 = QLabel("Entrer la valeur de N :", fen)
+text1.setGeometry(25, 25, 150, 20)
+
+
+lineEditN = QLineEdit(fen)
+lineEditN.setGeometry(150, 25, 200, 20)
+
 
 fen.show()
 app.exec()
