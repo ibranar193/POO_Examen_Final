a992d1f3ac5ef403ccf402041b076685e728d014
ibranar193 <mouadkadmiri@gmail.com>
2026-01-15 13:31:44 -0500

    premiere commit : creation de fichier

---
+++ Exercice 2.py
@@ -0,0 +1,10 @@
+# Importer la bibliotheque
+from PyQt6.QtWidgets import QApplication, QWidget
+
+app = QApplication([])
+fen = QWidget()
+fen.setWindowTitle("Doubleure")
+fen.setGeometry(100, 100, 400, 200)
+
+fen.show()
+app.exec()
