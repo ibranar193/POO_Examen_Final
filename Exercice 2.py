4aab54b2148e105b469cc1bb5ac98dcf0f49e854
ibranar193 <mouadkadmiri@gmail.com>
2026-01-15 14:02:36 -0500

    3 - commit : text2 + lineEdit 2 + fonction calculator

--- Exercice 2.py
+++ Exercice 2.py
@@ -1,6 +1,11 @@
 # Importer la bibliotheque
 from PyQt6.QtWidgets import QApplication, QWidget, QGridLayout, QLabel, QLineEdit
 
+def calculator () :
+    number = lineEditN.text()
+    result = number * 2
+    lineEditD.setText(result)
+
 app = QApplication([])
 fen = QWidget()
 fen.setWindowTitle("Doubleure")
@@ -11,6 +16,12 @@ text1.setGeometry(25, 25, 150, 20)
 
 lineEditN = QLineEdit(fen)
 lineEditN.setGeometry(150, 25, 200, 20)
+
+text2 = QLabel("Voici le double :", fen)
+text2.setGeometry(25, 50, 150, 20)
+
+lineEditD = QLineEdit(fen)
+lineEditD.setGeometry(150, 50, 200, 20)
 
 fen.show()
 app.exec()
