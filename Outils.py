cf0b114d1aa77753a128d61757f137b6bf3ade33
ibranar193 <mouadkadmiri@gmail.com>
2026-01-15 15:00:10 -0500

    2 - commit EX 3 : fix init + fonction max + dontion min

--- Outils.py
+++ Outils.py
@@ -1,10 +1,16 @@
+import numpy as np
 class Outils :
     def __init__(self) :
         self.entiers = []
-
+        self.array = np.array(self.entiers)
     def saisir (self) :
         print("Veuiller saisir 10 nombre entiers :")
         for i in range(10) :
             entier = int (input(f"Nombre {i+1} : "))
             self.entiers.append(entier)
 
+    def min(self) :
+        return self.array.min()
+
+    def max(self) :
+        return self.array.max()
